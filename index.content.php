40f953bb83f3941e277ace0348d348d5e1bcc80b
Wyatt Nolen <nolenw1@nku.edu>
2014-10-13 16:34:42 -0400

    Begin Boostrap
    Add bootstrap files; make small changes to include files.

--- index.content.php
+++ index.content.php
@@ -1,7 +1,5 @@
 <img src="images/banner.jpg" width="100%"/>
-<br /><br />
 <h1>Welcome to Organization Management</h1>
-
 <div class="left-float">
 <div class="third">
 	<h3>Create and Join Organizations</h3>
@@ -13,14 +11,12 @@
 	<h3>Create and Join Organizations</h3>
 </div>
 </div>
-
 <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature,</p> 
 
 <p>discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
 
 <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
 <!--<textarea>Your content here.</textarea>-->
-
 
 
 
--- templates/default/index.php
+++ templates/default/index.php
@@ -1,12 +1,12 @@
-
 <html>
     <head>
         <base href="<?php echo SITE_URL ?>">
         <link rel="stylesheet" type="text/css" href="<?php echo TEMPLATE_URL ?>css/style.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo TEMPLATE_URL ?>css/style.css" />
         <title><?php echo $title ?></title>
     </head>
     <body>
-        <div id="container">
+        <div id="pageWrapper">
             <div id="header">
                 <span id="logo"><a href="index.php">Organization Management</a></span>
                 <span id="logout">
@@ -21,7 +21,6 @@
             <div id="info">
                 <?php if(isset($content)) echo $content ?>
             </div>
-            <div style="clear: both;"></div>
         </div>
     </body>
 </html>

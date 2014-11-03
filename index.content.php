10f5c90a3c5a75d8787ed24ce72c1009699c060e
Wyatt Nolen <nolenw1@nku.edu>
2014-11-03 04:31:53 -0500

    Default Template Updated
    Still lots of more work to be done, but the front page is nearly done.

--- index.content.php
+++ index.content.php
@@ -1,22 +1,62 @@
-<img src="images/banner.jpg" width="100%"/>
-<h1>Welcome to Organization Management</h1>
-<div class="left-float">
-<div class="third">
-	<h3>Create and Join Organizations</h3>
+<div class="container-fluid" id="hero">
+    <div class="row">
+        <h1>Welcome to <br><span>Organization Management</span></h1>
+        <div class="col-xs-4 col-sm-4 col-md-4 m-featured-block">
+            <h2>Block 1</h2><!-- Join -->
+            <p>This is where a content block would go</p>
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-4 m-featured-block">
+            <h2>Block 2</h2><!-- Create -->
+            <p>This is where a content block would go</p>
+        </div>
+        <div class="col-xs-4 col-sm-4 col-md-4 m-featured-block">
+            <h2>Block 3</h2><!-- Manage -->
+            <p>This is where a content block would go</p>
+        </div>
+    </div>
 </div>
-<div class="third">
-	<h3>Management Features</h3>
+<div class="container-fluid" id="splash">
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-6">
+            <div class="row">
+                <ul class="col-xs-4 col-sm-4 col-md-4">
+                    <li class="is-bold">Col 1</li>
+                    <li>Col 1: Item 1</li>
+                    <li>Col 1: Item 2</li>
+                    <li>Col 1: Item 3</li>
+                    <li>Col 1: Item 4</li>
+                    <li>Col 1: Item 5</li>
+                </ul>
+                <ul class="col-xs-4 col-sm-4 col-md-4">
+                    <li class="is-bold">Col 2</li>
+                    <li>Col 2: Item 1</li>
+                    <li>Col 2: Item 2</li>
+                    <li>Col 2: Item 3</li>
+                    <li>Col 2: Item 4</li>
+                    <li>Col 2: Item 5</li>
+                </ul>
+                <ul class="col-xs-4 col-sm-4 col-md-4">
+                    <li class="is-bold">Col 3</li>
+                    <li>Col 3: Item 1</li>
+                    <li>Col 3: Item 2</li>
+                    <li>Col 3: Item 3</li>
+                    <li>Col 3: Item 4</li>
+                    <li>Col 3: Item 5</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6">
+            <h3 class="is-bold">Join, create, and manage organizations</h3>
+            <p>Here are some snazzy marketing words. What, what - is this more snazzy marketing words I spot? Yes it is. It is all about that content. Also, I should spell check this tomorrow. It is 4:17 am and I could not be more tired.</p>
+            <p>Now I am talking about why you should join our little site. Cause, bam, there are things in it for you, the consumer.</p>
+        </div>
+    </div>
+    <footer class="row">
+        <div id="copyright" class="col-xs-12 col-sm-6 col-md-6">
+            &copy; 2014 Organization Management. Handcrafted by <a target="_blank" href="#">Jesse Hockenbury &amp; Wyatt Nolen</a>
+        </div>
+    </footer>
 </div>
-<div class="third">
-	<h3>Create and Join Organizations</h3>
-</div>
-</div>
-<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature,</p> 
-
-<p>discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-
-<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-<!--<textarea>Your content here.</textarea>-->
 
 
 
--- templates/default/index.php
+++ templates/default/index.php
@@ -1,23 +1,54 @@
 <html>
     <head>
         <base href="<?php echo SITE_URL ?>">
-        <link rel="stylesheet" type="text/css" href="<?php echo TEMPLATE_URL ?>css/style.css" />
-        <link rel="stylesheet" type="text/css" href="<?php echo TEMPLATE_URL ?>css/style.css" />
         <title><?php echo $title ?></title>
+        
+        
+        <!-- styles -->
+        <link rel="stylesheet" type="text/css" href="<?php echo TEMPLATE_URL ?>css/normalize.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo TEMPLATE_URL ?>css/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo TEMPLATE_URL ?>css/style.css" />
+        
+        <!-- script -->
+        <script src="<?php echo TEMPLATE_URL ?>js/jquery-1.9.1.js" type='text/javascript'></script>
+        <script>
+            $(document).ready(function(){
+                $("#showHide").click(function(){
+                    $("#mobile-site-nav").toggleClass("show-right");
+                });
+            });
+	   </script>
     </head>
     <body>
         <div id="pageWrapper">
-            <div id="header">
-                <span id="logo"><a href="index.php">Organization Management</a></span>
-                <span id="logout">
+            <div id="mobile-site-nav" class="is-right">
+                <nav id="mobile-site-nav-inner">
+                    <a href="#">1</a>
                     <?php if(isset($_SESSION['user'])) :?>
                         <a href="membership.php?action=logout">Log Out</a>
                     <?php else : ?>
                         <a href="membership.php?action=login">Log In</a>
                     <?php endif;?>
-                </span> 
+                </nav>
             </div>
-            
+             <header id="site-header">
+                <div><h1 id="logo"><a href="http://www.thenortherner.com/">Organization Management</a></h1></div>
+                    <nav id="site-nav">
+                        <a href="#">1</a>
+                        <?php if(isset($_SESSION['user'])) :?>
+                            <a href="membership.php?action=logout">Log Out</a>
+                        <?php else : ?>
+                            <a href="membership.php?action=login">Log In</a>
+                        <?php endif;?>
+                    </nav>
+                    <button id="showHide" class="navbar-toggle">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+            </header>
+            <div id="site-header-holder"></div>
             <div id="info">
                 <?php if(isset($content)) echo $content ?>
             </div>
